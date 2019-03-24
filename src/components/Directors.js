b3f30c65806e5705e8c3586ca20329624da65598
cannoli9917 <katie.pennachio@gmail.com>
2019-03-24 17:07:00 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,35 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+
+  console.log(directors)
+
+  const renderDirectors = () => {
+    return directors.map(director => {
+
+      const movies = () => {
+        return director.movies.map(movie => {
+          return <li>{movie}</li>
+        })
+      }
+
+      return(
+        <div>
+          <h2>Name: {director.name}</h2>
+          <p>Movies:</p>
+          <ul>
+          {movies()}
+          </ul>
+        </div>
+      )
+    })
+  }
+
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {renderDirectors()}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,31 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
+  const renderMovies = () => {
+    return movies.map(movie => {
+      const genres = () => {
+        return movie.genres.map(genre => {
+        return <li>{genre}</li>
+      })}
+
+      return(
+        <div>
+        <h2>Name: {movie.title}</h2>
+        <p>Time: {movie.time}</p>
+        <p>Genres:</p>
+        <ul>
+          {genres()}
+        </ul>
+        </div>
+      )
+    })
+  }
+
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {renderMovies()}
     </div>
   );
 };
