5a9a0edeeca83505e0222f9af48929da8b4ed09e
github username <github email address>
2019-04-02 00:03:49 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import { directors } from '../data';
 
+// const renderDirectors = () => {
+//   return (
+//     <div>
+//       <h1> Directors Page </h1>
+//       {directors.map(director => (
+//         <div className='director'>
+//         <h3> Name: {director.name} </h3>
+//         <ul>
+//           {director.movies.map(movie => (
+//             <li> {movie} </li>
+//           ))}
+//           </ul>
+//       </div>
+//        ))}
+//     </div>
+//   )
+// }
+
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
-    </div>
-  );
+      <h1> Directors Page </h1>
+        {directors.map(director => (
+          <div className='director'>
+            <h3> Name: {director.name} </h3>
+            <ul>
+              {director.movies.map(movie => (
+                <li> {movie} </li>
+              ))}
+            </ul>
+            </div>
+          ))}
+      </div>
+    )
 }
+
 
 export default Directors
